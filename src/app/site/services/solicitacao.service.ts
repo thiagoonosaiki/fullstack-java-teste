f2bdbd4f9283cc4a9cda44bd87fbdb1e7936a6fe
thiagoonosaiki <thiago.yuugi1731@gmail.com>
2026-03-25 00:14:50 -0300

    Ajuste do conteúdo da tabela

--- src/app/site/services/solicitacao.service.ts
+++ src/app/site/services/solicitacao.service.ts
@@ -2,10 +2,29 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+export interface AereoSegmento {
+  cidadeOrigem: string;
+  cidadeDestino: string;
+}
+
+export interface Aereo {
+  cia: string;
+  localizador: string;
+  aereosSegmentos: AereoSegmento[];
+}
+
+export interface Passageiro {
+  nome: string;
+}
+  
 export interface Solicitacao {
   idSolicitacao: number;
+  status: string;
   solicitante: string;
+  passageiros: Passageiro[];
+  aereos: Aereo[];
 }
+
 
 @Injectable({
   providedIn: 'root'
